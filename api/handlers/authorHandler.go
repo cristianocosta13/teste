3161208e6b650567364e514a44fe7b76d5bfb8bc
cristiano <jccs5@aluno.ifal.edu.br>
2022-09-02 14:25:34 -0300

    alteracoes no diretorio handler

--- api/handlers/authorHandler.go
+++ api/handlers/authorHandler.go
@@ -16,14 +16,13 @@ func NewTodoHandler(authorUseCase interfaces.AuthorUseCase) *AuthorHandler {
 func (ah *AuthorHandler) Get() {
 	//id := req.PathParameter("id")
 	var id string
-	result, err := ah.authorUseCase.Get(id)
+	_, err := ah.authorUseCase.Get(id)
 	if err != nil {
-		resp.WriteError(500, err)
+		//resp.WriteError(500, err)
 		return
 	}
 
-	var todo *ToDo = &ToDo{}
+	//var todo *ToDo = &ToDo{}
 
-	todo.FromDomain(result)
-	resp.WriteAsJson(todo)
+	return
 }
--- api/handlers/handlerTest.go
+++ api/handlers/handlerTest.go
@@ -1,1 +1,30 @@
 package handlers
+
+import "teste/core/interfaces"
+
+type TestHandler struct {
+	authorUseCase interfaces.AuthorUseCase
+}
+
+func NewTodoHandle(authorUseCase interfaces.AuthorUseCase) *AuthorHandler {
+	handler := &AuthorHandler{
+		authorUseCase: authorUseCase,
+	}
+	return handler
+}
+
+func (ah *TestHandler) Get() int {
+	//id := req.PathParameter("id")
+	var id string
+	_, err := ah.authorUseCase.Get(id)
+	if err != nil {
+		//resp.WriteError(500, err)
+		return 8
+	}
+
+	//var todo *ToDo = &ToDo{}
+
+	//todo.FromDomain(result)
+	//resp.WriteAsJson(todo)
+	return 8
+}
